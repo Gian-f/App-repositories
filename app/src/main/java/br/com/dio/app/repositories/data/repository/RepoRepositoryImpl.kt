2462d3ba0c71067f32843544a0b2a5da59f64847
Gian-f <gian.silva@cialne.com.br>
2021-09-16 13:30:54 -0300

    Adding error's treatments

--- app/src/main/java/br/com/dio/app/repositories/data/repository/RepoRepositoryImpl.kt
+++ app/src/main/java/br/com/dio/app/repositories/data/repository/RepoRepositoryImpl.kt
@@ -1,12 +1,19 @@
 package br.com.dio.app.repositories.data.repository
 
+import br.com.dio.app.repositories.core.RemoteException
 import br.com.dio.app.repositories.data.services.GithubService
 import kotlinx.coroutines.flow.flow
+import retrofit2.HttpException
 
 class RepoRepositoryImpl(private val service: GithubService) : RepoRepository{
 
     override suspend fun ListRepositories(user : String) = flow {
-        val repoList = service.listRepositories(user)
-       emit(repoList)
+        try {
+            val repoList = service.listRepositories(user)
+            emit(repoList)
+        } catch(ex: HttpException) {
+            throw RemoteException(ex.message ?: "Usuário não encontrado")
+        }
+
     }
 }
